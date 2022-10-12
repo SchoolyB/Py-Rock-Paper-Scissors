a83acfc7f40c8628e55db51d72f1b4002710addb
Schooly <marshallbcodes@gmail.com>
2022-10-12 09:10:20 -0500

    Final commit/add comments

--- RPS.py
+++ RPS.py
@@ -1,18 +1,26 @@
+#VERSION 2.0.0
+#AUTHOR: Marshall Burns a.k.a Schooly B
+
+
+###IMPORTING THE 'RANDOM' LIBRARY TO ALLOW PYTHON TO MAKE RANDOM CHOICES###
 import random
+###IMPORTING THE 'SLEEP 'FUCNTION FROM THE 'TIME' LIBRARY TO ALLOW THE THE PROGRAM TO HAVE A TIME DELAY###
 from time import sleep
 
+###DECLARING 1ST VARIABLE TO BE USED WITHIN THE GAME() FUNTION. REPRESENTS USERS CHOICE###
 userAction = input("Enter your choice(Rock,Paper,or Scissors)")
 
+###DEFINING POSSOBLE CHOICES FOR COMPUTER###
 possibleCompActions = [
-
 "Rock", 
 "Paper",
 "Scissors",
 ]
 
+###DECLARING 2ND VARIABLE TO BE USED WITHIN THE GAME() FUNTION. REPRESENTS COMPUTERS CHOICE###
 compAction = random.choice(possibleCompActions)
 
-
+###DECLARING A PLAY_AGAIN()FUNCTION###
 def Play_Again():
 
     if playAgain == 'y':
@@ -21,8 +29,7 @@ def Play_Again():
     elif playAgain == 'n':
         print('Ok goodbye...') 
 
-
-
+###DECLARING A GAME() FUNCTION. THIS FUNCTION CONTAINS THE GAME ITSELF###
 def game():
 
     userAction = input("Enter your choice(Rock,Paper,or Scissors)")
@@ -35,7 +42,6 @@ def game():
     ]
 
     compAction = random.choice(possibleCompActions)
-
     
 
     if userAction == compAction:
@@ -80,10 +86,14 @@ def game():
             sleep(2)
             print ("Rock smashes scissors. YOU LOSE!!!")    
 
+
+###CALLING THE GAME() FUNCTION###
 game()
 
+###DECLARING A VARIABLE TO BE USED TO EXECUTE THE PLAY_AGAIN() FUNTION###
 playAgain = input('Would you like to play again? y/n ')
 
+###CALLING THE PLAY_AGAIN FUNCTION()###
 Play_Again()
 
 
